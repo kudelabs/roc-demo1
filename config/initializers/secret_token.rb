bc475c960c1b0eb359e362a6c5f894b0d602f231
Adeh Desandies <adeh@adehmac.local>
2011-04-27 22:43:45 +0800

    new app, initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rocweibo::Application.config.secret_token = '24c46f172913def3a12bd567fa1c8ca2ea2a261171322cb5ff2b48580ff7ecdf9abeab73e07a9661eb775594116c8ffde0b78d7d7ce1eb9e4dc6c0108aa80795'
